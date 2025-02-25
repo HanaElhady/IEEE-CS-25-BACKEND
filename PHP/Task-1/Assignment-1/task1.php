27491687e54859ac7f3caf2761ccffe8625d569c
HanaElhady <144938741+HanaElhady@users.noreply.github.com>
2025-02-26 00:24:40 +0200

    Create task1.php

---
+++ PHP/Task-1/Assignment-1/task1.php
@@ -0,0 +1,28 @@
+<?php
+/*
+   This is an example of a multi-line comment,
+   which can span multiple lines.
+*/
+?>
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>
+        <?= "My First PHP Page";
+        ?>
+    </title>
+  </head>
+  <body>
+    <div>
+    <?php echo"We Love" ;
+    ?>
+    </div>
+    <div>
+    <?php echo"Elzero Channel" ;
+    ?>
+    </div>
+  </body>
+</html>
