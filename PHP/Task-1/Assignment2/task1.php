c5bde80e281889b87486c281a8725a72be27f5fc
HanaElhady <144938741+HanaElhady@users.noreply.github.com>
2025-02-26 01:58:09 +0200

    Create task1.php

---
+++ PHP/Task-1/Assignment2/task1.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="Elzero Courses">
+    <title>
+        <?php 
+        $message = "Elzero Courses";
+         ?>
+         <?="Welcome To $message" ; ?></title>
+  </head>
+  <body>
+    <h1><?=$message; ?></h1>
+    <p>Here In <?=$message; ?> We Provide Front-End And Back-End Courses</p>
+    <hr>
+    <div> <?=$message; ?> Is The What You Need.</div>
+    <footer>All Right Reserved To  <?=$message; ?></footer>
+  </body>
+</html>
