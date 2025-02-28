acaf2708b5135bc4ecac898d2884a42c97245ee5
HanaElhady <144938741+HanaElhady@users.noreply.github.com>
2025-02-28 16:33:17 +0200

    Create task2.php

---
+++ PHP/Task-2/Assignment2/task2.php
@@ -0,0 +1,25 @@
+<?php
+
+$index = 0;
+
+for (; $index <= 20; $index+=2) {
+    if($index > 0 ) {
+        echo $index . '<br>' ;
+}
+    
+}
+echo"<br>";
+$index = 0;
+while($index < 20  ) {
+    $index +=2;
+    echo $index . '<br>' ;
+}
+echo"<br>";
+$index =0;
+
+do{
+    $index +=2;
+    echo $index . '<br>' ;
+}while($index < 20 )
+?>
+
