cb2f2e07f8491fc5efed92acaa13abc3dbb0f388
HanaElhady <144938741+HanaElhady@users.noreply.github.com>
2025-02-28 15:01:18 +0200

    Create task7.php

---
+++ PHP/Task-2/Assignment-1/task7.php
@@ -0,0 +1,25 @@
+<?php
+
+$num_one = 23;
+$num_two = 5;
+$op = "/"; 
+
+switch ($op) {
+    case '+':
+        echo $num_one + $num_two;
+        break;
+    case '-':
+        echo $num_one - $num_two;
+        break;
+    case '*':
+        echo $num_one * $num_two;
+        break;
+    case '/':
+        echo intdiv($num_one, $num_two) . "<br>"; 
+        echo $num_one % $num_two; 
+        break;
+    default:
+        echo "Unknown Operation"; 
+}
+
+?>
