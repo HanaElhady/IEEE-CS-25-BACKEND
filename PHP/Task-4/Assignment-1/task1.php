c1aa41d45bac02beb1b1c09aaccf67c74dab2dc8
HanaElhady <144938741+HanaElhady@users.noreply.github.com>
2025-03-06 16:52:30 +0200

    Update task1.php

--- PHP/Task-4/Assignment-1/task1.php
+++ PHP/Task-4/Assignment-1/task1.php
@@ -1,1 +1,8 @@
-<?php ?>
+<?php
+
+$time = time() + (86400) * 65;
+setcookie("site[layout]","boxed", $time , "/");
+setcookie("site[font]","Swat", $time , "/");
+setcookie("site[color]","blue", $time , "/");
+
+?>
